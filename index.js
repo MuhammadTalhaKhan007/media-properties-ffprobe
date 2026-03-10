f50832e35301e66f8d849b618d09d52d90c871bc
Muhammad Talha Khan <talha.khan@persivia.com>
2026-03-10 16:43:12 +0500

    Added more samples

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const ffprobe = require("ffprobe");
 const ffprobeStatic = require("ffprobe-static");
 
-const filePath = "./Call-Center-Audio.mp3"; // Change me
+const medicalFilePath = "./Complex-Medical-Dictation-(AI).mp3";
+const callCenterFilePath = "./Call-Center-Audio-MP3.mp3";
 
-ffprobe(filePath, { path: ffprobeStatic.path })
+ffprobe(medicalFilePath, { path: ffprobeStatic.path })
   .then((info) => {
     const stream = info.streams[0];
 
     const audioInfo = {
+      medicalAudio: "Complex-Medical-Dictation-(AI).mp3",
+      codec: `${stream.codec_name}`,
+      sampleRate: `${stream.sample_rate} Hz`,
+      channels: `${stream.channels}`,
+      duration: `${stream.duration} seconds`,
+      bitRate: `${stream.bit_rate} bps`,
+    };
+
+    console.log(audioInfo);
+  })
+  .catch((err) => {
+    console.error("Error:", err);
+  });
+
+ffprobe(callCenterFilePath, { path: ffprobeStatic.path })
+  .then((info) => {
+    const stream = info.streams[0];
+
+    const audioInfo = {
+      callCenterAudio: "Call-Center-Audio.mp3",
       codec: `${stream.codec_name}`,
       sampleRate: `${stream.sample_rate} Hz`,
       channels: `${stream.channels}`,
